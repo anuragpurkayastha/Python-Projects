49901b69aac2397ebb1f63cd4aae3fc069cea19d
Anurag Purkayastha <anuragpurkayastha@gmail.com>
2020-06-17 14:22:42 +1000

    Updated Get Bin Days script to include Point Cook

--- Get Bin Days/getBinDays.py
+++ Get Bin Days/getBinDays.py
@@ -1,5 +1,5 @@
 '''
-    This is a script that scrapes the council website to find the bin days.
+    This is a script that scrapes the Melton and Point Cook council websites to find the bin collection days.
 
     It makes use of the selenium package to automate browser activities.
 
@@ -15,6 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located as EC
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 print("Gathering bin collection information...\n")
 
@@ -26,9 +27,10 @@ driverOpts.add_argument('-headless')
 driver = webdriver.Firefox(options = driverOpts)
 wait = WebDriverWait(driver,10) #   Wait 10 seconds for page elements to load.
 
+#       MELTON
 #   Search for the residential address and wait for the response
 try:
-    #   Go to the council's rubbish collection website.
+    #   Go to the Melton council's rubbish collection website.
     driver.get('https://www.melton.vic.gov.au/Near-Me')
 
     #   Wait until the search input bar is loaded.
@@ -43,41 +45,74 @@ try:
     wait.until(EC((By.CLASS_NAME,'intramaps-waste__day')))
 
     #   Get the elements that contain info about all the collection days
-    collectionDaysList = driver.find_elements_by_class_name('intramaps-waste__day')
+    wasteDayElements = driver.find_elements_by_class_name('intramaps-waste__day')
 
     # The first 3 elements of collection days elements are the 'Garbage', 'Recycling', 'Green Waste' dates. Create a dictionary with the waste type as the key and the HTML elements as the values.
-    collectionDays = {'Garbage': collectionDaysList[0],
-        'Recycling': collectionDaysList[1],
-        'Green Waste': collectionDaysList[2]}
+    wasteDays_MEL_HTML = {'Garbage': wasteDayElements[0].get_attribute('outerHTML'),
+        'Recycling': wasteDayElements[1].get_attribute('outerHTML'),
+        'Green Waste': wasteDayElements[2].get_attribute('outerHTML')}
 
 except Exception as exc:
     print("Error: " + str(exc))
 
-#   For each collectionDay item, create 'caption' and 'value' dictionaries, which contain the text for the caption and value, respectively.
-#   e.g collectionDay_caption = {'Garbage':['Collection Day','Waste Area'],...}
-collectionDay_captions = {}
-collectionDay_values = {}
+#   Create a waste info dictionary for Melton. This has 'waste type' as the key, and the information related to that waste type as a value which is also a dictionary.
+#   e.g {'Garbage':{'Next date':'22 June 2020', 'Waste Area': 'A'}}
+wasteDayInfo_MEL = {}
 
-#   For each element in the 'collectionDays' dictionary, get the HTML code of the values, parse them through BeautifulSoup, and then save the content of the HTML code to corresponding lists.
-for key in collectionDays.keys():
+#   For each HTML element, get the 'caption' and 'value' and store them in the dictionary
+for wasteType in wasteDays_MEL_HTML.keys():
 
-        soup = BeautifulSoup(collectionDays[key].get_attribute('outerHTML'),
+        soup = BeautifulSoup(wasteDays_MEL_HTML[wasteType],
             'html.parser')
 
-        #   Get the element captions
+        #   Get the caption and value elements from the current HTML element
         captions = soup.find_all(class_='intramaps-waste__caption')
         values = soup.find_all(class_='intramaps-waste__value')
 
-        collectionDay_captions[key] = []
-        collectionDay_values[key] = []
+        #   Temp dictionary to store in the main dictionary. {key: 'caption', value: 'value'}
+        wasteDayInfo_values = {}
 
+        #   For each caption-value pair, save it into the dictionary 'wasteDayInfo_values'
         for i in range(len(captions)):
-            collectionDay_captions[key].append(captions[i].string)
-            collectionDay_values[key].append(values[i].string)
+            wasteDayInfo_values[captions[i].string] = values[i].string
 
-for key in collectionDay_captions.keys():
-    print(key.upper())
+        wasteDayInfo_MEL[wasteType] = wasteDayInfo_values
 
-    for i in range(len(collectionDay_captions[key])):
-        print('%s:\t%s' % (collectionDay_captions[key][i],collectionDay_values[key][i]))
-    print('=' * 80)
+#       POINT COOK
+#   Get the dates for Point Cook
+driver.get('https://digital.wyndham.vic.gov.au/myWyndham/?propnum=187817&radius=3000&mapfeatures=')
+
+try:
+    wait.until(EC((By.CLASS_NAME,'waste')))
+
+    #   Get all the 'waste info elements'
+    waste_info_elements = driver.find_elements_by_class_name('waste')
+
+    wasteDayInfo_PC = {}
+
+    #   The first 3 elements contain the appropriate info
+    for elem in waste_info_elements[:3]:
+        soup = BeautifulSoup(elem.get_attribute('outerHTML'),'html.parser')
+
+        #   Get the string
+        splitString = soup.string.split(':')
+        wasteDayInfo_PC[splitString[0]] = splitString[1]
+
+except Exception as exc:
+    print('Error: %s' % exc)
+
+#           Print the results for melton
+print('MELTON'.center(80))
+for wasteType in wasteDayInfo_MEL.keys():
+    print('\t'+wasteType.upper())
+
+    for wasteInfoVal in wasteDayInfo_MEL[wasteType]:
+        print('\t%s:\t%s' % (wasteInfoVal, wasteDayInfo_MEL[wasteType][wasteInfoVal]))
+    print('\t'+'=' * 80)
+
+print('\t'+'#' * 80)
+
+print('\n'+'POINT COOK'.center(80)+'\n')
+for garbageType in wasteDayInfo_PC.keys():
+    print('\t%s:\t%s' % (garbageType, wasteDayInfo_PC[garbageType]))
+    print('\t'+'=' * 80)
