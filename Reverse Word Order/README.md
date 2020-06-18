a68e2da67360039f90565aac91f3017b6a7bb00b
Anurag Purkayastha <anuragpurkayastha@gmail.com>
2020-06-18 13:30:06 +1000

    Cleaned up Reverse Word Order

---
+++ Reverse Word Order/README.md
@@ -0,0 +1,7 @@
+#Reverse Word Order
+A program that takes an input sentence and reverses the word order.
+
+#Usage
+```python
+python ReverseWordOrder.py
+```
--- Reverse Word Order/ReverseWordOrder.py
+++ Reverse Word Order/ReverseWordOrder.py
@@ -8,14 +8,14 @@ shown back to me."""
 
 def reverse_word_order(string):
 	""" Function reverses the word order of the input 'string' and returns the result"""
-	
+
 	stringSplit = string.split() # Split the string using whitespace as the delimiter
-	
+
 	reversed = stringSplit[::-1] # Reversed the word order of the string
-	
+
 	return " ".join(reversed)
 
 # Ask the user for input string
-inputString = input("Please enter a string to be reversed: ")
+inputString = input("Please enter a multiple word sentence to be reversed: ")
 # Reverse the string and print out the result
 print ("The reversed string is: ",reverse_word_order(inputString),"\n")
