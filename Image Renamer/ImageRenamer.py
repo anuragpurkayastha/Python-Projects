699c05ed9aecd23264188d7dc9269313870e96f1
anuragpurkayastha <anuragpurkayastha@gmail.com>
2020-06-24 12:12:38 +1000

    Updated ImageRenamer.py

--- Image Renamer/ImageRenamer.py
+++ Image Renamer/ImageRenamer.py
@@ -5,22 +5,31 @@
 
 #   Import modules
 import os
-from pathlib import Path
+from pathlib import Path, PureWindowsPath
 from PIL import Image
-from pprint import pprint
 import re
 
-'''
-    Function that returns a new filename given a given image PATH (absolute)
-'''
-def getNewFilename(imagePath):
-    #   Get the filebasename and extension
-    fileBasename = os.path.split(imageFilePath)[1]
-    extension = os.path.splitext(fileBasename)[1]
+def getNewFilename(absImagePath, seqNo):
+    '''
+        Function that returns a new filename.
+        Arguments:
+            -   absImagePath:  Absolute path to the image as a Path object.
+            -   seqNo:   The sequence number
+        Returns:
+            -   A string containing the new filename in the format <suffix>_<yyyy_mm_dd>_<seqNo>.<ext>
+            If the 'Date Taken' attribute is not able to be found, then a default renaming scheme is applied.
+    '''
+    #   Get the basename of the file and the extension
+    basename = os.path.basename(absImagePath)
+    fileExt = os.path.splitext(basename)[1]
+    
+    #   Leading filename text - CHANGE ACCORDING TO NEEDS
+    leadingText = 'CHANGE LEADING TEXT OF FILE'
     
     try:
-        image = Image.open(imageFilePath)      #   Open the image to extract the EXIF data
-        image_date = image._getexif()[36867]    #   Get the datetime field in the EXIF data - returns 'yyyy:mm:dd hh:mm:ss' string
+        #   Open the image to extract the EXIF data and get the date the picture was taken
+        image = Image.open(absImagePath)
+        image_date_taken = image._getexif()[36867]    #   Get the datetime field in the EXIF data - returns 'yyyy:mm:dd hh:mm:ss' string
 
         #   Create a regex to extract the year, month and day from the image_date
         imageDateRegex = re.compile(r'''(
@@ -33,44 +42,48 @@ def getNewFilename(imagePath):
             )''',re.VERBOSE)
 
         #   Search for the data in the EXIF data of the current image.
-        image_date_re = imageDateRegex.search(image_date)
+        image_date_re = imageDateRegex.search(image_date_taken)
 
         #   Extract the year, month and day
         imageYear = image_date_re.group(2)
         imageMonth = image_date_re.group(3)
         imageDay = image_date_re.group(4)
 
-        newFilename = f'Aneeka_First Week_{imageYear}_{imageMonth}_{imageDay}{extension}'
-    except KeyError:
-        print(f'{fileBasename} - No EXIF data found.')
-        newFilename = f'Aneeka_First Week{extension}'
+        #   Construct the new filename appending the date to the end of it.
+        newFilename = f'{leadingText}_{imageYear}_{imageMonth}_{imageDay}_{seqNo}{fileExt}'
+        
+    except Exception as error:
+        '''
+            If an exception occurs apply a default name to the filename.
+        '''
+        print(f'Error opening file: {str(error)}. Applying default renaming...')
+        newFilename = f'{leadingText}_{seqNo}{fileExt}'
     finally:
+        #   Return the new filename.
         return newFilename
 
-#   TODO - Loop through all the images in a directory
-IMAGE_DIR = Path("D:/Pictures/2020/March/Aneeka's Album/First Month/1 week photo shoot")
-
-image_files = IMAGE_DIR.glob('*.jpg')
-
-#   Get the image date - Must get it from the EXIF data.
-for i, imageFilePath in enumerate(image_files):
-    print(f'{i+1} - ',end=' ')
-    newFilename = getNewFilename(imageFilePath)
-    fileBasename = os.path.split(imageFilePath)[1]
-    fileDirectory = os.path.dirname(imageFilePath)
+#   MAIN
+if __name__ == '__main__':
     
-    #   Calculate the new file path for the new file name
-    newFilePath = Path(os.path.join(fileDirectory,newFilename))
-    
-    if newFilePath.exists():
-       newFilename_base = os.path.splitext(os.path.split(newFilePath)[1])[0]
-       newFilename_ext = os.path.splitext(os.path.split(newFilePath)[1])[1]
-       
-       newFilename = f'{newFilename_base}_{i}{newFilename_ext}'
-       
-       newFilePath = os.path.join(os.path.dirname(imageFilePath),newFilename)
-       
-    print(f'Renaming {fileBasename} to {newFilename}...')
-    imageFilePath.rename(newFilePath)
+    #   Directory containing the images to rename - CHANGE AS REQUIRED
+    windowsPath = PureWindowsPath(r"/path/to/directory/")   #   Required on Windows platforms.
+    IMAGE_DIR = Path(windowsPath.as_posix())
 
-#   TODO - Rename the image to 'Aneeka_First Week_<YYYY_MM_DD>.jpg'
+    #   Loop through each item in the directory and rename the item if it is a file.
+    for i, imageFile in enumerate(os.listdir(IMAGE_DIR)):
+        imageFilePath = IMAGE_DIR/imageFile
+        
+        #   If the current item is a directory then pass.
+        if (imageFilePath).is_dir():
+            continue
+            
+        print(f'Image {i+1} - ',end=' ')
+        
+        #   Get a new filename for the current image
+        newFilename = getNewFilename(imageFilePath,i+1)
+        
+        #   Calculate the new file path for the new file name
+        newFilePath = IMAGE_DIR/newFilename
+           
+        print(f'Renaming {imageFile} to {newFilename}...')
+        imageFilePath.rename(newFilePath)
