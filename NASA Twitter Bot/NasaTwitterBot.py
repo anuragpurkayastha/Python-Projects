a4c6760219f5b860216d40bb4579e7a25a6d0241
anuragpurkayastha <anuragpurkayastha@gmail.com>
2020-07-26 15:17:48 +1000

    Changes to twitter bots and image renamer

--- NASA Twitter Bot/NasaTwitterBot.py
+++ NASA Twitter Bot/NasaTwitterBot.py
@@ -1,69 +1,72 @@
 """
-	This script gets the Picture of the Day from the NASA website.
+    This script gets the Picture of the Day from the NASA website.
 """
 import json, requests, os, tweepy
 from pprint import pprint
 
 def getNasaPOTD():
-	"""
-		This function queries the NASA API and downloads the POTD
-	"""
+    """
+        This function queries the NASA API and downloads the POTD
+    """
 
-	#	Get the API key
-	api_key = os.getenv('NASA_API_KEY')
+    #   Get the API key
+    api_key = os.getenv('NASA_API_KEY')
 
-	params = {
-		'api_key': str(api_key),
-		'hd': 'true'
-	}
+    params = {
+        'api_key': str(api_key),
+        'hd': 'true'
+    }
 
-	url = 'https://api.nasa.gov/planetary/apod'
+    url = 'https://api.nasa.gov/planetary/apod'
 
-	res = requests.request('GET', url, params=params)
-	res.raise_for_status()
+    res = requests.request('GET', url, params=params)
+    res.raise_for_status()
 
-	resData = json.loads(res.text)
+    resData = json.loads(res.text)
 
-	imageTitle = resData['title']
-	imageURL = resData['url']
-	imageFilename = os.path.basename(imageURL)
+    imageTitle = resData['title']
+    imageURL = resData['url']
+    imageFilename = os.path.basename(imageURL)
 
-	res = requests.request('GET', imageURL)
-	res.raise_for_status()
+    res = requests.request('GET', imageURL)
+    res.raise_for_status()
 
-	with open(imageFilename,'wb') as file:
-		for chunk in res.iter_content(100000):
-			file.write(chunk)
+    with open(imageFilename,'wb') as file:
+        for chunk in res.iter_content(100000):
+            file.write(chunk)
 
-	#	Return info about the image
-	return {'filename':imageFilename, 'title': imageTitle}
+    #   Return info about the image
+    return {'filename':imageFilename, 'title': imageTitle}
 
 def postToTwitter(filename, status):
-	"""
-		Function that posts the picture to Twitter with the given 'status'
-	"""
+    """
+        Function that posts the picture to Twitter with the given 'status'
+    """
 
-	#	Create a Tweepy object - Get the credentials from environment variables.
-	api_key = os.getenv("TWITTER_API_KEY")
-	api_secret = os.getenv("TWITTER_API_SECRET")
-	access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-	access_token_secret = os.getenv("TWITTER_ACCESS_SECRET")
+    #   Create a Tweepy object - Get the credentials from environment variables.
+    api_key = os.getenv("TWITTER_API_KEY")
+    api_secret = os.getenv("TWITTER_API_SECRET")
+    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.getenv("TWITTER_ACCESS_SECRET")
 
-	auth = tweepy.OAuthHandler(api_key, api_secret)
-	auth.set_access_token(access_token, access_token_secret)
-	api = tweepy.API(auth, wait_on_rate_limit=True,
-	    wait_on_rate_limit_notify=True)
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
 
-	api.update_with_media(filename, status)
+    api.update_with_media(filename, status)
 
 if __name__ == '__main__':
+    
+    #   Change the working directory to the home directory
+    os.chdir(os.path.expanduser('~'))
+    
+    #   Grab the link and download the POTD from NASA
+    potd = getNasaPOTD()
 
-	#	Grab the link and download the POTD from NASA
-	potd = getNasaPOTD()
+    #   Post the image to twitter with a status.
+    postToTwitter(potd['filename'],
+        f"Check out today's Picture of the Day from NASA!\n\n{potd['title']}:")
 
-	#	Post the image to twitter with a status.
-	postToTwitter(potd['filename'],
-		f"Check out today's Picture of the Day from NASA!\n\n{potd['title']}:")
-
-	#	Remove the downloaded image
-	os.remove(potd['filename'])
+    #   Remove the downloaded image
+    os.remove(potd['filename'])
